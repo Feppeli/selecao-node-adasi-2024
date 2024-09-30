8788d8e3519ee698babaa586917e00653f0687e5
Felipe <dfsilvat@gmail.com>
2024-09-30 02:54:39 -0300

    comment: comentado código de middlewares

--- src/midlewares/activitiesValidations.js
+++ src/midlewares/activitiesValidations.js
@@ -1,3 +1,4 @@
+// Validação para verificar se a hora final da atividade é menor que a hora inicial
 const isvalidateActivity = (date, start_activitie, end_activitie) => {
     const startDateTime = new Date(`${date}T${start_activitie}`);
     const endDateTime = new Date(`${date}T${end_activitie}`);
@@ -5,7 +6,7 @@ const isvalidateActivity = (date, start_activitie, end_activitie) => {
     return startDateTime < endDateTime
 }
 
-
+// validação para verificar se a hora final da atividade é no máximo 6 horas maior que a hora inicial
 const isWithinSixHour = (date, start_activitie, end_activitie) => {
     const start = new Date(`${date}T${start_activitie}`);
     const end = new Date(`${date}T${end_activitie}`);
