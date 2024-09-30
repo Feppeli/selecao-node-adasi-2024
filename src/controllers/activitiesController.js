de4fdf49f86a1479f02bbd0c6ba10c1c17c57abb
Felipe <dfsilvat@gmail.com>
2024-09-30 01:51:52 -0300

    build: first validate for date and hour

--- src/controllers/activitiesController.js
+++ src/controllers/activitiesController.js
@@ -1,5 +1,4 @@
 const Activities = require('../database/models/activities')
-
 // functions for controller the API
 
 // get all activities 
@@ -30,34 +29,67 @@ const getActivitie = async (req, res) => {
 
 }
 
+
+
 // Add Activitie
-const addActivitie = async (req, res) => {
-    const activitie = await Activities.create(req.body);
+const addActivitie = async (req, res) => {  
+
+    const {date, start_activitie, end_activitie} = req.body
 
 
-    const id = parseInt(activitie.id)
-    const task = activitie.id
-    const student = activitie.student
-    const start = activitie.start_activitie
-    const end = activitie.end_activitie
 
-    //opitional
-    const beginning = activitie.beginning_activie
-    const finish = activitie.finish_activitie
+    const isvalidateActivity = (date, start_activitie, end_activitie ) => {
+        const startDateTime = new Date(`${date}T${start_activitie}`);
+        const endDateTime = new Date(`${date}T${end_activitie}`);
 
-    try {
-        res.status(201).send({
-            id,
-            task,
-            student,
-            start,
-            end,
-            beginning,
-            finish
-        })
-    }catch(err) {
-        res.status(404).send("Ocorreu algum erro: " + err)
+        return startDateTime < endDateTime
     }
+
+    if(!isvalidateActivity(date, start_activitie, end_activitie)){
+        return res.status(400).send('A hora de término deve ser após a hora de início.');
+    }else{
+        
+    }
+    
+
+    try{
+        await Activities.create(req.body)
+    .then((activitie) => {
+        res.status(201).send(activitie)
+    })
+    .catch(err =>{
+        res.status(400).send("Ocorreu algum erro:" + err)
+    })
+    }
+    catch(err){
+        res.status(400).send("ocorreu algum erro" + err)
+    }
+
+
+    // const id = parseInt(activitie.id)
+    // const task = activitie.id
+    // const student = activitie.student
+    // const start = activitie.start_activitie
+    // const end = activitie.end_activitie
+
+    // //opitional
+    // const beginning = activitie.beginning_activie
+    // const finish = activitie.finish_activitie
+
+    // try {
+    //     res.status(201).send({
+    //         id,
+    //         date,
+    //         task,
+    //         student,
+    //         start,
+    //         end,
+    //         beginning,
+    //         finish
+    //     })
+    // }catch(err) {
+    //     res.status(404).send("Ocorreu algum erro: " + err)
+    // }
 }
 
 // Delete Student
